825db1f714bf99231960200aa69411f1bfab65d5
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-05-29 16:52:08 -0400

    poll for cron over eventsub

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
-import process from "node:process";
 import Cron from "croner";
 import { Bot } from "@twurple/easy-bot";
-import { EventSubWsListener } from "@twurple/eventsub-ws";
 import havok from "../data/havok.json";
 import ai from "./ai";
 import aistats from "./aistats";
 import { auth } from "./auth";
-import { redis } from "./redis";
 import { log } from "./util";
 
 const bot = new Bot({
@@ -17,26 +14,13 @@ const bot = new Bot({
 
 bot.onConnect(() => log.info(`Connected to Twitch`));
 
-const ws = new EventSubWsListener({ apiClient: bot.api });
-
-const job = new Cron(
+Cron(
 	"*/30 * * * *",
 	async () => {
+		const stream = await bot.api.streams.getStreamByUserName("Gladd");
+		if (!stream) return;
+
 		await bot.say("Gladd", havok[(Math.random() * havok.length) | 0]);
-		log.info("Cron job triggered");
 	},
-	{ paused: true, timezone: "America/New_York" },
+	{ timezone: "America/New_York" },
 );
-
-const online = (await redis.get("online")) === "1";
-if (online) job.resume();
-
-ws.onStreamOnline(process.env.TWITCH_CHANNEL_ID!, async () => {
-	job.resume();
-	await redis.set("online", "1");
-});
-
-ws.onStreamOffline(process.env.TWITCH_CHANNEL_ID!, async () => {
-	job.pause();
-	await redis.set("online", "0");
-});
