3fca06dbd6fc2a73f967fddd17ddb8f860edb743
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-05-30 16:23:11 -0400

    fix consecutive phrases

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Cron from "croner";
 import { Bot } from "@twurple/easy-bot";
-import havok from "../data/havok.json";
+import phrases from "../data/havok.json";
 import commands from "./commands";
 import { auth } from "./auth";
 import { log } from "./util";
@@ -13,13 +13,18 @@ const bot = new Bot({
 
 bot.onConnect(() => log.info(`Connected to Twitch`));
 
+for (let i = phrases.length - 1; i > 0; i--) {
+	const j = (Math.random() * (i + 1)) | 0;
+	[phrases[i], phrases[j]] = [phrases[j], phrases[i]];
+}
+
 Cron(
 	"*/30 * * * *",
 	async () => {
 		const stream = await bot.api.streams.getStreamByUserName("Gladd");
 		if (!stream) return;
 
-		await bot.say("Gladd", havok[(Math.random() * havok.length) | 0]);
+		await bot.say("Gladd", phrases[(Math.random() * phrases.length) | 0]);
 	},
 	{ timezone: "America/New_York" },
 );
