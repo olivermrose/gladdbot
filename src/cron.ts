cae005133251f9c4a409fbfcded2080392a0bfd6
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-06-26 20:28:06 -0400

    add random content to cron

---
+++ src/cron.ts
@@ -0,0 +1,32 @@
+import process from "node:process";
+import type { Bot } from "@twurple/easy-bot";
+import phrases from "../data/phrases.json";
+import { model } from "./model";
+import { redis } from "./redis";
+import { sanitize } from "./util";
+
+const interval = Number(process.env.CRON_JOB_INTERVAL);
+
+for (let i = phrases.length - 1; i > 0; i--) {
+	const j = (Math.random() * (i + 1)) | 0;
+	[phrases[i], phrases[j]] = [phrases[j], phrases[i]];
+}
+
+export async function job(bot: Bot) {
+	const stream = await bot.api.streams.getStreamByUserName("Gladd");
+	if (!stream) return;
+
+	const intervals = await redis.incr("intervals");
+	if (intervals < interval / 5) return;
+
+	if (Math.random() > 0.5) {
+		const { response } = await model.generateContent(
+			"Give a hot take/unpopular opinion, insult a random mod/user, or say something random that would trigger people.",
+		);
+
+		await bot.say("Gladd", sanitize(response.text(), { limit: 350 }));
+	}
+
+	await bot.say("Gladd", phrases[(Math.random() * phrases.length) | 0]);
+	await redis.set("intervals", 0);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import Cron from "croner";
 import { Bot } from "@twurple/easy-bot";
-import phrases from "../data/phrases.json";
 import commands from "./commands";
 import { auth } from "./auth";
-import { redis } from "./redis";
+import { job } from "./cron";
 import { log } from "./util";
 
 const bot = new Bot({
@@ -14,22 +13,4 @@ const bot = new Bot({
 
 bot.onConnect(() => log.info(`Connected to Twitch`));
 
-for (let i = phrases.length - 1; i > 0; i--) {
-	const j = (Math.random() * (i + 1)) | 0;
-	[phrases[i], phrases[j]] = [phrases[j], phrases[i]];
-}
-
-Cron(
-	"*/15 * * * *",
-	async () => {
-		const stream = await bot.api.streams.getStreamByUserName("Gladd");
-		if (!stream) return;
-
-		const intervals = await redis.incr("intervals");
-		if (intervals !== 3) return;
-
-		await bot.say("Gladd", phrases[(Math.random() * phrases.length) | 0]);
-		await redis.set("intervals", 0);
-	},
-	{ timezone: "America/New_York" },
-);
+Cron(`*/5 * * * *`, async () => await job(bot), { timezone: "America/New_York" });
