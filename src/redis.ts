7ef642160be07bb73b7661df0ba5613308897dae
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-07-22 23:58:04 -0400

    log error

--- src/redis.ts
+++ src/redis.ts
@@ -2,3 +2,7 @@ import process from "node:process";
 import { createClient } from "redis";
 
 export const redis = await createClient({ url: process.env.REDIS_URL }).connect();
+
+redis.on("error", (error) => {
+	console.error(error);
+});
