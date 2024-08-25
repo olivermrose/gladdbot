7e1eeea630afd009321e566c7d4b4b4b6762363e
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-08-25 00:11:25 -0400

    handle 429s on cron

--- src/cron.ts
+++ src/cron.ts
@@ -2,7 +2,7 @@ import process from "node:process";
 import type { Bot } from "@twurple/easy-bot";
 import { model } from "./model";
 import { redis } from "./redis";
-import { sanitize } from "./util";
+import { handleError, sanitize } from "./util";
 
 const interval = Number(process.env.CRON_JOB_INTERVAL);
 
@@ -13,12 +13,16 @@ export async function job(bot: Bot, messages: string[]) {
 	const intervals = await redis.incr("intervals");
 	if (intervals < interval / 5) return;
 
-	const { response } = await model.generateContent(`
-		Respond to ONLY ONE of these messages. DO NOT promote sponsors.
-		===================
-		${messages.join("\n")}
-	`);
+	try {
+		const { response } = await model.generateContent(`
+			Respond to ONLY ONE of these messages. DO NOT promote sponsors.
+			===================
+			${messages.join("\n")}
+		`);
 
-	await bot.say("Gladd", sanitize(response.text(), { limit: 350 }));
-	await redis.set("intervals", 0);
+		await bot.say("Gladd", sanitize(response.text(), { limit: 350 }));
+		await redis.set("intervals", 0);
+	} catch (error) {
+		handleError(error);
+	}
 }
