07139280938f5adf16b40f863791b808b8488ae6
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-09-08 17:05:03 -0400

    revert

--- src/cron.ts
+++ src/cron.ts
@@ -15,7 +15,7 @@ export async function job(bot: Bot, messages: string[]) {
 
 	try {
 		const { response } = await model.generateContent(`
-			Respond to ONLY ONE of these messages. DO NOT promote sponsors. DO NOT repeat any of the messages.
+			Respond to ONLY ONE of these messages.
 			===================
 			${messages.join("\n")}
 		`);
