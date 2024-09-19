ce1373f6d5ff01c55de9aa42f58fd4b8056755cd
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-09-18 21:05:29 -0400

    more flash nudging

--- src/cron.ts
+++ src/cron.ts
@@ -15,7 +15,7 @@ export async function job(bot: Bot, messages: string[]) {
 
 	try {
 		const { response } = await model.generateContent(`
-			Respond to ONLY ONE of these messages.
+			Respond to ONLY ONE of these messages without repeating what it said.
 			===================
 			${messages.join("\n")}
 		`);
