01b31e54613aee4ff132c1fe7a8c8669d2502100
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-10-21 23:46:27 -0400

    pp

--- src/commands/aiping.ts
+++ src/commands/aiping.ts
@@ -3,7 +3,7 @@ import { defineCommand } from "../util";
 // Only useful to do a quick check if the bot's alive and not use an api request
 export default defineCommand({
 	name: "aiping",
-	exec(_, ctx) {
-		return ctx.reply("fuck u lol buhOverShakeyFlipExplode");
+	async exec(_, ctx) {
+		await ctx.reply("fuck u lol buhOverShakeyFlipExplode");
 	},
 });
