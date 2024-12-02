7ce8ad918accd9e47599e0ecbc141c6292afa32b
xiBread <oscar.leevermeren@gmail.com>
2024-12-02 10:55:30 -0500

    include tag responses in stats

--- src/commands/aistats.ts
+++ src/commands/aistats.ts
@@ -4,8 +4,11 @@ import { defineCommand } from "../util";
 export default defineCommand({
 	name: "aistats",
 	async exec(_, ctx) {
-		const responseCount = await redis.get("responses");
+		const responses = Number(await redis.get("responses") ?? 0);
+		const responsesTags = Number(await redis.get("responses_tag") ?? 0);
 
-		await ctx.reply(`I've responded to ${responseCount ?? 0} prompts Chatting`);
+		const total = responses + responsesTags
+
+		await ctx.reply(`I've responded to ${total} prompts Chatting`);
 	},
 });
