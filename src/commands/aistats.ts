db0709234fbdd35589ad572180aa10cfa5dc78a8
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-12-15 20:30:08 -0500

    update redis key

--- src/commands/aistats.ts
+++ src/commands/aistats.ts
@@ -4,10 +4,10 @@ import { defineCommand } from "../util";
 export default defineCommand({
 	name: "aistats",
 	async exec(_, ctx) {
-		const responses = Number(await redis.get("responses") ?? 0);
-		const responsesTags = Number(await redis.get("responses_tag") ?? 0);
+		const responses = Number((await redis.get("responses")) ?? 0);
+		const responsesMention = Number((await redis.get("responses_mention")) ?? 0);
 
-		const total = responses + responsesTags
+		const total = responses + responsesMention;
 
 		await ctx.reply(`I've responded to ${total} prompts Chatting`);
 	},
